51ac2a16d9b41c8cc772bdfda4f5fa1dbe4f6823
ahmetpergamum <ahmet.pergamum@gmail.com>
2018-01-15 17:57:22 +0300

    ao

--- proxy.sh
+++ proxy.sh
@@ -11,19 +11,19 @@ echo "Proxy Password:"
 read -s PASSW
 
 
-FILE="~/.bashrc"
-# dosyada kullanici sifre iceren proxy patterni var mi
-grep "://.*@.*[0-9]:[0-9]" $FILE &> /dev/null
-# varsa sadece kullanici adi sifre ve sunucu bilgilerini degistir
-if [ $? -eq 0 ]
-then
-	sudo sed -i "s/\:\/\/.*@.*$/\:\/\/$USERNAME:$PASSW\@$SERVER:$PORT\//" $FILE
-	source $FILE
-else
- 	sudo sed -i "\$aexport http_proxy=http://$USERNAME.$PASSW@$SERVER:$PORT" $FILE
- 	sudo sed -i "\$aexport https_proxy=https://$USERNAME.$PASSW@$SERVER:$PORT" $FILE
-	source $FILE
-fi
+# FILE="~/.bashrc"
+# # dosyada kullanici sifre iceren proxy patterni var mi
+# grep "://.*@.*[0-9]:[0-9]" $FILE &> /dev/null
+# # varsa sadece kullanici adi sifre ve sunucu bilgilerini degistir
+# if [ $? -eq 0 ]
+# then
+# 	sudo sed -i "s/\:\/\/.*@.*$/\:\/\/$USERNAME:$PASSW\@$SERVER:$PORT\//" $FILE
+# 	source $FILE
+# else
+#  	sudo sed -i "\$aexport http_proxy=http://$USERNAME.$PASSW@$SERVER:$PORT" $FILE
+#  	sudo sed -i "\$aexport https_proxy=https://$USERNAME.$PASSW@$SERVER:$PORT" $FILE
+# 	source $FILE
+# fi
 
 FILE="/etc/wgetrc"
 # dosyada kullanici sifre iceren proxy patterni var mi
@@ -45,16 +45,16 @@ if [ $? -eq 0 ]
 then
 	sudo sed -i "s/\:\/\/.*@.*$/\:\/\/$USERNAME:$PASSW\@$SERVER:$PORT\//" $FILE
 else
-	sudo sed -i "\$ahttp_proxy="http://$USERNAME:$PASSW@$SERVER:$PORT/"" $FILE
-	sudo sed -i "\$ahttps_proxy="http://$USERNAME:$PASSW@$SERVER:$PORT/"" $FILE
-	sudo sed -i "\$aftp_proxy="http://$USERNAME:$PASSW@$SERVER:$PORT/"" $FILE
-	sudo sed -i "\$asocks_proxy="socks://$USERNAME:$PASSW@$SERVER:$PORT/"" $FILE
-	sudo sed -i "\$ano_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"" $FILE
-	sudo sed -i "\$aHTTP_PROXY="http://$USERNAME:$PASSW@$SERVER:$PORT/"" $FILE
-	sudo sed -i "\$aHTTPS_PROXY="http://$USERNAME:$PASSW@$SERVER:$PORT/"" $FILE
-	sudo sed -i "\$aFTP_PROXY="http://$USERNAME:$PASSW@$SERVER:$PORT/"" $FILE
-	sudo sed -i "\$aSOCKS_PROXY="socks://$USERNAME:$PASSW@$SERVER:$PORT/"" $FILE
-	sudo sed -i "\$aNO_PROXY="localhost,127.0.0.1,localaddress,.localdomain.com"" $FILE
+	sudo sed -i "\$ahttp_proxy=\"http://$USERNAME:$PASSW@$SERVER:$PORT/\"" $FILE
+	sudo sed -i "\$ahttps_proxy=\"http://$USERNAME:$PASSW@$SERVER:$PORT/\"" $FILE
+	sudo sed -i "\$aftp_proxy=\"http://$USERNAME:$PASSW@$SERVER:$PORT/\"" $FILE
+	sudo sed -i "\$asocks_proxy=\"socks://$USERNAME:$PASSW@$SERVER:$PORT/\"" $FILE
+	sudo sed -i "\$ano_proxy=\"localhost,127.0.0.1,localaddress,.localdomain.com\"" $FILE
+	sudo sed -i "\$aHTTP_PROXY=\"http://$USERNAME:$PASSW@$SERVER:$PORT/\"" $FILE
+	sudo sed -i "\$aHTTPS_PROXY=\"http://$USERNAME:$PASSW@$SERVER:$PORT/\"" $FILE
+	sudo sed -i "\$aFTP_PROXY=\"http://$USERNAME:$PASSW@$SERVER:$PORT/\"" $FILE
+	sudo sed -i "\$aSOCKS_PROXY=\"socks://$USERNAME:$PASSW@$SERVER:$PORT/\"" $FILE
+	sudo sed -i "\$aNO_PROXY=\"localhost,127.0.0.1,localaddress,.localdomain.com\"" $FILE
 fi
 FILE="/etc/apt/apt.conf"
 # dosyada kullanici sifre iceren proxy patterni var mi
@@ -64,10 +64,11 @@ if [ $? -eq 0 ]
 then
 	sudo sed -i "s/\:\/\/.*@.*$/\:\/\/$USERNAME:$PASSW\@$SERVER:$PORT\//" $FILE
 else
-	sudo sed -i "\$aAcquire::http::proxy "http://$USERNAME:$PASSW@$SERVER:$PORT/";" $FILE
-	sudo sed -i "\$aAcquire::https::proxy "https://$USERNAME:$PASSW@$SERVER:$PORT/";" $FILE
-	sudo sed -i "\$aAcquire::ftp::proxy "ftp://$USERNAME:$PASSW@$SERVER:$PORT/";" $FILE
-	sudo sed -i "\$aAcquire::socks::proxy "socks://$USERNAME:$PASSW@$SERVER:$PORT/";" $FILE
+	echo "# apt configuration file" | sudo tee -a $FILE > /dev/null
+	sudo sed -i "\$aAcquire::http::proxy \"http://$USERNAME:$PASSW@$SERVER:$PORT/\";" $FILE
+	sudo sed -i "\$\$aAcquire::https::proxy \"https://$USERNAME:$PASSW@$SERVER:$PORT/\";" $FILE
+	sudo sed -i "\$aAcquire::ftp::proxy \"ftp://$USERNAME:$PASSW@$SERVER:$PORT/\";" $FILE
+	sudo sed -i "\$aAcquire::socks::proxy \"socks://$USERNAME:$PASSW@$SERVER:$PORT/\";" $FILE
 fi
 
 FILE="~/.gitconfig"
