2b75a2e19e3fd15a08c02098c33d2a8e8354ad40
ahmetpergamum <ahme.pergamum@gmail.com>
2018-05-04 10:31:10 +0300

    ao

--- cert.sh
+++ cert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ca-certificate configuration script 
+# ca-certificate configuration script
 # run the script at the directory that the .pem file resides
 
 pemFile=$(ls *.pem)
@@ -10,4 +10,9 @@ openssl x509 -in $pemFile -inform PEM -out $certFile
 sudo mkdir /usr/share/ca-certificates/$certName
 sudo cp $certFile /usr/share/ca-certificates/$certName/.
 sudo dpkg-reconfigure ca-certificates
+
+# change pip conf file
+sudo echo "[global]" >> /etc/pip.conf
+sudo echo "cert = /usr/share/ca-certificates/$certName/$certFile" >> /etc/pip.conf
+
 echo Done!...
