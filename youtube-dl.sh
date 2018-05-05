f186d10eff7de6ebf26fad930db316c45b6a7728
ahmetpergamum <ahmet.pergamum@gmail.com>
2018-05-05 16:27:12 +0300

    ao

---
+++ youtube-dl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+dir=$(pwd)
+if [ $# -eq 0 ]; then
+
+	while read line
+	do
+		youtube-dl -o "$dir/%(title)s.%(ext)si" -i -x --audio-format mp3 $line
+		#youtube-dl -o '%(title)s.%(ext)s' --restrict-filenames -i -x --audio-format mp3 $line
+	done<$dir/liste
+elif [ $1 == "-v" ]; then
+	while read line
+	do
+		youtube-dl -f mp4 -o "$dir/%(title)s.%(ext)s" -i  $line --verbose --no-check-certificate
+		#youtube-dl -f mp4 -o '/home/admin1/Music/%(title)s.%(ext)s' -i  $line
+	done<$dir/liste
+else
+	echo "Usage for video download	: $0 -v"
+	echo "for audio download	: $0"
+fi
