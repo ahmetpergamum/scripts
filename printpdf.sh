b1aea96a50561a6a630e11b9aa0d3d8c27303dc9
ahmetpergamum <ahmet.pergamum@gmail.com>
2018-05-12 02:15:00 +0300

    ao

--- printpdf.sh
+++ printpdf.sh
@@ -7,7 +7,21 @@
 pageN=$(pdftk "$1" dump_data | grep NumberOfPages | awk '{print $2}')
 echo $pageN
 
-for i in $(seq 1 $pageN)
+if [ $((pageN%2)) -eq 0 ]
+then
+	c=$((pageN/2))
+else
+	c=$(((pageN-1)/2))
+fi
+
+for i in $(seq 0 $c)
 do
-	echo $i
+	echo $((i * 2))
 done
+
+# if []
+# then
+# 	lp -P $EVENS $1
+# elif[]
+# 	lp -P $ODDS $1
+# fi
